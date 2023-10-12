7472debbad270a78a6a5d08bdcdde8179f9424c6
samwelmakokha <samwelmakokha10@gmail.com>
2023-10-11 17:47:26 -0700

    data structures: set, dict

---
+++ 0x04-python-more_data_structures/101-main.py
@@ -0,0 +1,14 @@
+guillaume@ubuntu:~/0x04$ cat 101-main.py
+#!/usr/bin/python3
+square_matrix_map = \
+    __import__('101-square_matrix_map').square_matrix_map
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_map(matrix)
+print(new_matrix)
+print(matrix)
